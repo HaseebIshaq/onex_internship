393678452d1579530f92c6b935c56f7a85c0acdc
DESKTOP-ENSLE99 <haseeb.ishaq1144@gmail.com>
2023-06-21 13:46:57 +0500

    commit

--- Haseeb_Dashboard/sp.py
+++ Haseeb_Dashboard/sp.py
@@ -4,53 +4,68 @@ import streamlit as st
 
 # -- Import and clean data (importing csv into pandas)
 df = pd.read_csv("sales_data.csv")
+print(df.info())
 dff = df.copy()
+
+# Adding another column of Discounted Amount
 df['Discounted Amount'] = df['Sale Price'] - df['Price Sold']
+# Dealing Missing Values
 df['Store Location'].fillna('Other', inplace=True)
 
 # App layout
 st.markdown("<h1 style='font-size: 24px; text-align: center;'>Web Application Dashboards for the sales data </h1>", unsafe_allow_html=True)
 st.markdown("---")
 
-# Filter options
-# date_options = ['1/2/2023', '1/3/2023', '1/4/2023', '1/5/2023']
+# Filter options (Drop Down Options)
+# date_options = pd.to_datetime(df['Date']).dt.date.unique()
 date_options = df['Date'].unique()
 all_locations = ['New York', 'San Francisco', 'Nevada', 'Los Angeles']
 
-# Sidebar filters
+# Sidebar filters (removing/adding filters with checkboxes)
 st.sidebar.markdown("## Filters")
 date_filter = st.sidebar.checkbox("Select Date")
-location_filter = st.sidebar.checkbox("Filter by Store Location")
+location_filter = st.sidebar.checkbox("Select Store Location")
 
 selected_location = None
-selected_date = None
+start_date = None
+end_date = None
 
+# When checkbox marked, then Dropdown appears
 if location_filter:
     selected_location = st.sidebar.selectbox(
         "Select Store Location", all_locations)
 
 if date_filter:
-    selected_date = st.sidebar.selectbox('Select Date', date_options)
+    st.sidebar.markdown("### Select Date Range")
+    start_date = st.sidebar.selectbox('Start Date', date_options)
+    end_date = st.sidebar.selectbox('End Date', date_options)
 
-# Display filter selections
+# Display filter selections in side margin
 st.sidebar.markdown("### Filter Selections")
-st.sidebar.write("Selected Date:", selected_date)
+st.sidebar.write("Start Date:", start_date)
+st.sidebar.write("End Date:", end_date)
 st.sidebar.write("Selected Store Location:", selected_location)
 
 filtered_data = df
 
-# Apply filters
-if selected_date and selected_location:
-    filtered_data = df[(df["Date"] == selected_date) & (
-        df["Store Location"] == selected_location)]
-elif selected_date:
-    filtered_data = filtered_data[filtered_data["Date"] == selected_date]
-elif selected_location:
-    filtered_data = filtered_data[filtered_data["Store Location"]
+# Apply filters w.r.t. location and date possibilities
+if start_date and end_date:
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+    filtered_data = df[(pd.to_datetime(df['Date']).dt.date >= start_date) & (
+        pd.to_datetime(df['Date']).dt.date <= end_date)]
+elif start_date:
+    start_date = pd.to_datetime(start_date)
+    filtered_data = filtered_data[pd.to_datetime(
+        filtered_data['Date']).dt.date >= start_date]
+elif end_date:
+    end_date = pd.to_datetime(end_date)
+    filtered_data = filtered_data[pd.to_datetime(
+        filtered_data['Date']).dt.date <= end_date]
+
+if selected_location:
+    filtered_data = filtered_data[filtered_data['Store Location']
                                   == selected_location]
-else:
-    st.markdown("<h2 style='font-size: 20px; text-align: center;'>DataFrame of Sales Data, which is used for this analysis. </h2>", unsafe_allow_html=True)
-    st.write(dff)  # Display the DataFrame with details
 
 # Graphs w.r.t. Date
 fig1 = px.bar(
@@ -61,7 +76,6 @@ fig1 = px.bar(
     title='No. of Products sold from Each Location on A Given Date',
     color_discrete_sequence=['#FFA500']
 )
-# col1.plotly_chart(fig1)
 
 fig2 = px.line(
     filtered_data.groupby('Store ID')['Price Sold'].sum().reset_index(),
@@ -71,7 +85,6 @@ fig2 = px.line(
     markers=True,
     title='Amount of Sales from Each Store on A Given Date'
 )
-# col1.plotly_chart(fig2)
 
 fig3 = px.bar(
     filtered_data['Product ID'].value_counts().reset_index(),
@@ -81,7 +94,6 @@ fig3 = px.bar(
     title='Particular Product Sale on A Given Date',
     color_discrete_sequence=['#f44336']
 )
-# col1.plotly_chart(fig3)
 
 fig4 = px.bar(
     filtered_data.groupby('Store Location')['Price Sold'].sum().reset_index(),
@@ -90,7 +102,6 @@ fig4 = px.bar(
     title='Sales of Particular Amount w.r.t. Location on a Given Date',
     color_discrete_sequence=['#1e5008']
 )
-# col2.plotly_chart(fig4)
 
 fig5 = px.pie(
     filtered_data.groupby('Store Location')[
@@ -133,15 +144,7 @@ fig9 = px.bar(
     color_discrete_sequence=['#1e5008']
 )
 
-# fig10 = px.bar(
-#     filtered_data.groupby('Date')['Product ID'].value_counts().reset_index(),
-#     x='Date',
-#     y='Product ID',
-#     labels={'Product ID':'Total No. of Products'},
-#     title='Total No. of Products Sold on each Date from a Given Location',
-#     color_discrete_sequence=['#FFA500']
-# )
-
+# Graphs when Both filters applied (Also include fig2)
 fig10 = px.pie(
     filtered_data['Store ID'].value_counts().reset_index(),
     names='index',
@@ -157,10 +160,33 @@ fig11 = px.bar(
     title='No. of products sold'
 )
 
-# Create a container for the grid layout
+# Neutral Graphs
+fig12 = px.pie(
+    filtered_data['Product ID'].value_counts().reset_index(),
+    values='Product ID',
+    names='index',
+    title='Top Selling Products'
+)
+
+fig13 = px.pie(
+    filtered_data['Store Location'].value_counts().reset_index(),
+    names='index',
+    values='Store Location',
+    title='Top Locations'
+)
+
+fig14 = px.pie(
+    filtered_data['Store ID'].value_counts().reset_index(),
+    names='index',
+    values='Store ID',
+    title='Top Stores'
+)
+
+# Create a container for the grid layout 
 container = st.container()
 
-if selected_date and selected_location:
+# Displaying Graphs w.r.t. selection of filters (conditional)
+if start_date and end_date and selected_location:
     with container:
         col1, col2, col3 = st.columns(3)
         with col1:
@@ -169,7 +195,7 @@ if selected_date and selected_location:
             st.plotly_chart(fig11, use_container_width=True)
         with col3:
             st.plotly_chart(fig2, use_container_width=True)
-elif selected_date:
+elif start_date and end_date:
     with container:
         col1, col2, col3 = st.columns(3)
         with col1:
@@ -197,3 +223,12 @@ elif selected_location:
             st.plotly_chart(fig8, use_container_width=True)
         with col2:
             st.plotly_chart(fig9, use_container_width=True)
+else:
+    with container:
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.plotly_chart(fig12, use_container_width=True)
+        with col2:
+            st.plotly_chart(fig13, use_container_width=True)
+        with col3:
+            st.plotly_chart(fig14, use_container_width=True)
